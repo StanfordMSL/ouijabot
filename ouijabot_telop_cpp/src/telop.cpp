01c0ec1d3c7b462f5c6686238378b75823784b72
Kunal Shah <kshah.kunal@gmail.com>
2019-02-04 10:17:10 -0800

    added persistent velocity pub for the telop node

--- ouijabot_telop_cpp/src/telop.cpp
+++ ouijabot_telop_cpp/src/telop.cpp
@@ -9,10 +9,16 @@
 // class declaration
 class Telop{
 private:
-  ros::NodeHandle node_handle_; // the underscore is a tradition for notating private variables in a class
+  ros::NodeHandle nh_; // the underscore is a tradition for notating private variables in a class
   ros::Subscriber joy_subscriber_;
   ros::Publisher velocity_publisher_;
   geometry_msgs::Twist velocity_;
+
+
+  //timer assets 
+  double controlLoopFreq_; // frequency for control loop
+  ros::Timer controlTimer_;
+  void controlTimerCB(const ros::TimerEvent& event);
 
 public:
   Telop(); // constructor
@@ -24,10 +30,17 @@ public:
 Telop::Telop()
 {
   // create subscriber and bind callback function
-  joy_subscriber_ = node_handle_.subscribe<sensor_msgs::Joy>("/joy", 20, &Telop::joystick_callback, this); // must pass in: 1. the callback function; 2. the object instance by "this" pointer
+  joy_subscriber_ = nh_.subscribe<sensor_msgs::Joy>("/joy", 20, &Telop::joystick_callback, this); // must pass in: 1. the callback function; 2. the object instance by "this" pointer
 
   // create velocity publisher
-  velocity_publisher_ = node_handle_.advertise<geometry_msgs::Twist>("vel_out", 10);
+  velocity_publisher_ = nh_.advertise<geometry_msgs::Twist>("vel_out", 10);
+
+  // create main loop timer
+  ros::param::get("~control_freq", controlLoopFreq_);
+
+  controlTimer_ = nh_.createTimer(
+      ros::Duration(1.0/controlLoopFreq_), 
+      &Telop::controlTimerCB, this); 
 }
 
 void Telop::joystick_callback(const sensor_msgs::JoyConstPtr &joy)
@@ -38,11 +51,12 @@ void Telop::joystick_callback(const sensor_msgs::JoyConstPtr &joy)
   velocity_.linear.x = - joy->axes[0]; 
   velocity_.linear.y = joy->axes[1];
   velocity_.angular.z = joy->axes[2];
+}
 
+void Telop::controlTimerCB(const ros::TimerEvent& event) {
   // publish the velocity command (Twist type)
   velocity_publisher_.publish(velocity_);
 }
-
 
 // main function
 int main(int argc, char **argv)
