a75cb4097b749b31d3d6ea652a500809c9154c61
Kunal Shah <kshah.kunal@gmail.com>
2019-03-15 15:28:37 -0700

    proxy class for robot control (#2)
    * fixed python setup issues
    
    * added some error handling and user friendly
    
    * minor changes, tests passed
    
    * made pose and vel topics a param. flight tested robot approved

--- README.md
+++ README.md
@@ -1,26 +1,37 @@
 # ouijabot
 Omnidirectional robot developed at Multi-robot Systems Lab, Stanford University
 
-### Robot side: use package "ouijabot"
+### Robot package: "ouijabot"
 - ssh into the robot
 - Check /etc/hosts to see if the master IP has been added
 - Edit ~/.bahsrc so that the following two lines reflect the robot's own IP as well as the master IP
 ```
 export ROS_HOSTNAME=ouijabot1 # robot's own hostname/IP
-export ROS_MASTER_URI=http://msl-Lenovo:11311 # PC hostname/IP
+export ROS_MASTER_URI=http://relay.local:11311 # PC hostname/IP
 ```
 - In **ouijabot/launch/ouijabot.launch**, edit the number in **group ns="ouijabot1"** if you need to change the index of the robot
 - catkin_make the package
 - `source ./catkin_ws/devel/setup.bash`
-- Fire `roslaunch ouijabot ouijabot.launch` and ready to go!
-- **Important**: please poweroff the Raspberry Pi (run `sudo poweroff` in ssh)  before turning off the Ouijabot
+- Run `robot_up` and ready to go!
+- **Important**: please poweroff the Raspberry Pi (run `robot_down` in ssh)  before turning off the Ouijabot
 
 
-### Laptop side: use package "ouijabot_telop_cpp"
+### User package: "ouijabot_telop_cpp"
 - Check /etc/hosts and ~/.bashrc like we did for the robot
 - In **ouijabot_telop_cpp/launch/telop.launch**, change the index in **remap from="vel_out" to="/ouijabot1/cmd_vel"** if need to control different ouijabots
-- Fire `roslaunch ouijabot_telop_cpp telop.launch` and enjoy driving the Ouijabot!
+- Launch `roslaunch ouijabot_telop_cpp telop.launch` and enjoy driving the Ouijabot!
 - Optionally, you can use `roslaunch ouijabot_telop_cpp telop_mux.launch` to control multiple ouijabots using one joystick! Hold button 7,8,9,10 to select ouijabot 1,2,3,4, or hold button 12 to select all the robots.
+
+### User package: "ouijabot_proxy"
+ - use '`from ouijabot_proxy.ouijabotProxy import OuijabotProxy` to import the proxy class
+ - create an instance of the proxy by the constructor `ouijabot = OuijabotProxy("/<ns>/raw/pose", "/<ns>/cmd_vel")` where `<ns>` is the namespace of the robot
+ - `ouijabot.setEnable(True)` to move the robot
+ - `ouijabot.setVelocityTarget(vel, frame='R')` to set the body frame velocity of the robot. Use `frame='W'` for world frame
+ - `ouijabot.setPoseTarget(pose)` to set the 2D pose of the robot, where pose = (x, y, theta)
+ - `ouijabot.getPose()` to get the 2D pose. Optional arg `full=True` will return a ros POSE message
+
+
+
 
 ### IMU Calibration
 - The IMU calibration (gyro and accelerometer) will be performed automatically upon robot bootup. The calibration will start 3 seconds after the robot is powered on, and will take a few more seconds to finish.
---
+++ ouijabot_proxy/src/ouijabot_proxy/ouijabotProxy.py
@@ -0,0 +1,300 @@
+#!/usr/bin/env python
+
+'''
+The MIT License (MIT)
+Copyright (c) 2019 Kunal Shah
+                kshah.kunal@gmail.com
+'''
+#std lib imports 
+
+import sys
+import time
+import numpy as np
+import numpy.linalg as la
+
+# Standard ROS message
+import rospy
+import std_msgs.msg 
+from geometry_msgs.msg import Pose, PoseStamped, Twist
+from sensor_msgs.msg import Imu
+import tf.transformations as transf 
+
+
+# Custom ROS message
+from ouijabot.msg import Wheel_Spd # custom ROS msg for wheel spd (4 floats)
+from ouijabot.msg import Current # custom ROS msg for motor current (4 floats)
+from ouijabot.srv import Enable_IMU # enable uploading IMU measurement
+from ouijabot.srv import Enable_Current # enable uploading current measurement
+
+def Rot2d(alpha):
+    #2d rotation matrix 
+    return np.array([[np.cos(alpha), -np.sin(alpha)], [np.sin(alpha), np.cos(alpha)]])
+
+
+class OuijabotProxy(object):
+    """This class implements a proxy for the comunication beteween the user and the ouijabot 
+        purposes/usage: 
+            handle position/location information from motion capture
+            velocity (twist) set point following with respect to frame transforms
+
+
+        the final output will always be a twist to the ouijabot onboard process
+    """
+    def __init__(self, postionTopic, velocityTopic, params=None, mode="vel"):
+        #postionTopic optitrack or filtered position data 
+        #velocityTopic ouijabot output topic 
+        #main serial comm
+        #ros node
+        
+
+        #parms
+        if params is None:
+            self.velMax_l= rospy.get_param('~velMax_l')
+            self.velMax_a= rospy.get_param('~velMax_a')
+            self.cmdFrq= rospy.get_param('~cmdFrq')
+            self.ID= rospy.get_param('~id')
+        else:
+            self.velMax_l= params['velMax_l']
+            self.velMax_a= params['velMax_a']
+            self.cmdFrq= params['cmdFrq']
+            self.ID = params['ID']
+
+        self.modes = ['vel', 'pos']
+        self.mode =mode 
+
+        #pub 
+        self.cmdPub = rospy.Publisher(velocityTopic, Twist, queue_size=15)
+
+        #subs
+        rospy.Subscriber(postionTopic, PoseStamped, self.poseCB)
+
+        #intialize
+        self.pose = None
+        self.poseTarget = None
+        self.twistCmd = Twist()
+        self.enable = False
+
+        counter = 0 
+        while self.pose is None:
+            #wait for pose 
+            rospy.loginfo("Attempting to Establish Position Lock")
+            rospy.sleep(1)
+            counter +=1
+            if counter > 5:
+                raise RuntimeError("Position Lock Failure")
+
+        rospy.loginfo("Ouijabot Position Established")
+
+
+        #timers
+        self.controlTimer = rospy.Timer(rospy.Duration(1./self.cmdFrq), self.controlLoop)
+
+        rospy.loginfo("Ouijabot Initialization Exited Successfully: Clear for Launch")
+
+    # call back from optirack data
+    def poseCB(self, msg):
+        #get pose form topic 
+        self.pose=msg.pose
+
+    def setMode(self, mode):
+        if mode not in self.modes:
+            self.stop()
+            self.enable=False
+            raise RuntimeError("mode not found. terminating")
+        self.mode= mode
+
+    def getPose(self, full=False):
+        #return the pose of the robot, if full = True return the full Pose message struct 
+        if full:
+            return self.pose
+        else:
+            Q= [self.pose.orientation.x, 
+                self.pose.orientation.y, 
+                self.pose.orientation.z, 
+                self.pose.orientation.w]
+
+            euler =  transf.euler_from_quaternion(Q)
+            return (self.pose.position.x, self.pose.position.y, euler[2])
+
+    def setPoseTarget(self, pose):
+        #sets the target pose (world frame) of the robot
+        #pose is a (x, y, theta) NOT A ROS MSG POSE
+        self.mode="pose"
+
+        #check for angle 
+        theta= pose[2] 
+        #frist wrap to 2 pi
+        if theta> 2*np.pi or theta < 0:
+            theta= pose[2]%(2*np.pi)
+        #then to -pi, pi
+        if theta >= np.pi:
+            theta = 2*np.pi - theta
+
+        self.poseTarget = (pose[0], pose[1], theta) #pose is x, y, theta 
+
+
+    def poseControl(self):
+        kp_lin = 1
+        kp_ang = .08
+
+        poseCurrent= self.getPose()
+        #get velocity in world frame
+        dx = self.poseTarget[0] - poseCurrent[0]
+        dy = self.poseTarget[1] - poseCurrent[1]
+        dtheta = self.circularDist(self.poseTarget[2], poseCurrent[2])
+
+        v_world = [dx, dy] #2D planar velocity 
+        #convert to body frame
+        v_body = self.frameTransformR(v_world, poseCurrent[2])
+
+
+        vel = [v_body[0]*kp_lin,
+               v_body[1]*kp_lin, 
+               dtheta*kp_ang] 
+
+        self.setVelocityTarget(vel)
+
+    def circularDist(self, a1, a2):
+        #returns the difference between two angles, a counting for wrapping
+        #assumes the angles are in [-pi, pi]
+        assert a1>-np.pi and a1<=np.pi
+        assert a2>-np.pi and a2<=np.pi
+
+        diff = ( a1 - a2 + np.pi ) % (2*np.pi) - np.pi;
+        return diff + (2*np.pi) if diff < -np.pi else diff 
+
+    def getPoseTargetDist(self):
+        if self.poseTarget is None:
+            raise RuntimeError("goal position not set")
+
+        poseCurrent= self.getPose()
+        dx = self.poseTarget[0] - poseCurrent[0]
+        dy = self.poseTarget[1] - poseCurrent[1]
+
+        return (dx**2+ dy**2)**.5
+
+
+    def frameTransformR(self, v, a, reverse=False):
+        #transforms the vector v (in frame A) by the transformaton given by angle a (A to B) into frame B
+        #assumes that v is a list
+        R=Rot2d(a)
+        if reverse:
+            return np.dot(R, np.array(v)) 
+        else:
+            return np.dot(R.T, np.array(v))
+
+    def frameTransformQ(self, v, q, reverse=False):
+
+        #NOT YET TESTED
+        #transforms the vector v (in frame A) by the transformaton given by quaternion q (A to B) into frame B
+        #assumes that v and q are lists
+
+        v_ = [v[0], v[1], 0.0, 0.0] # "real" component of quaternions in ros is w, which is the 4th element. turn vector into a pure (no real part) quaternion
+
+        # "conjugate" vector v1 by quaternion q1 (i.e. apply the rotation to v1) - https://en.wikipedia.org/wiki/Quaternions_and_spatial_rotation
+        v_ = tf.transformations.quaternion_multiply(
+                     tf.transformations.quaternion_multiply(q, v_),
+                     tf.transformations.quaternion_inverse(q) # should be same as tf.transformations.quaternion_conjugate(q1), assuming q1 is a unit quaternion
+                    )[:3]
+        return v_
+
+    def setVelocityTarget(self, vel, frame='R'):
+        #frame 'R" sets the target velocity of the robot (v_x, v_y, v_theta) in robot frame
+        if frame == 'R':
+
+            self.twistCmd.linear.x= vel[0]
+            self.twistCmd.linear.y= vel[1]
+            self.twistCmd.angular.z= vel[2]
+        #frame 'W" sets the target velocity of the robot (v_x, v_y, v_theta) in world frame 
+        elif frame =='W':
+            poseCurrent= self.getPose()
+            #get velocity in world frame
+            dx = vel[0]
+            dy = vel[1]
+            v_world = [dx, dy] #2D planar velocity 
+            #convert to body frame
+            v_body = self.frameTransformR(v_world, poseCurrent[2])
+            self.twistCmd.linear.x= v_body[0]
+            self.twistCmd.linear.y= v_body[1]
+            self.twistCmd.angular.z= vel[2]
+        else:
+            self.stop()
+            raise RuntimeError('invalid frame for velocity control')
+
+        #clip to tresholds
+        self.clipVelocity()
+
+    def clipVelocity(self):
+        velMag_l = la.norm([self.twistCmd.linear.x, self.twistCmd.linear.y])
+        velMag_a = abs(self.twistCmd.angular.z)
+        if velMag_l > self.velMax_l:
+            self.twistCmd.linear.x *= (self.velMax_l/velMag_l)
+            self.twistCmd.linear.y *= (self.velMax_l/velMag_l)
+
+        if velMag_a > self.velMax_a:
+            self.twistCmd.angular.z *= (self.velMax_a/velMag_a)
+            
+    def getVelocityTarget(self):
+        #returns the current velocity setpoint
+        return (self.twistCmd.linear.x,
+                self.twistCmd.linear.y,
+                self.twistCmd.angular.z)
+
+    def setEnable(self, enb):
+        #enb is bool
+        self.enable = enb
+
+    def stop(self):
+        #stops the robot 
+        self.twistCmd = Twist() #resets the robot
+
+    def controlLoop(self, event):
+        if self.enable:
+            if self.mode=="pose":
+                self.poseControl()
+        else:
+            self.stop()
+        self.cmdPub.publish(self.twistCmd)
+
+
+
+
+def testVelocity(bot):
+    vels=[(1, 0, 0 ),
+          (0, -1, 0 ),
+          (-1, 0, 0 ),
+          (0, 1, 0 ),
+          (0, 0, .05 ),
+          (0, 0, -.05 )]
+    times= [1, 1, 1, 1, 1, 1 ]
+
+    for vel, t in zip(vels, times):
+        startTime = time.time()
+        while (time.time()- startTime)< t:
+            bot.setVelocityTarget(vel,  frame ='W')
+            position = bot.getPose()
+            velocity = bot.getVelocityTarget()
+            print("\ncurrent pose: {:2.4f}, {:2.4f}; {:2.4f} ".format(position[0],position[1], position[2]))
+            print("cmd velocity: {:2.4f}, {:2.4f}; {:2.4f} ".format(velocity[0],velocity[1], velocity[2]))
+            time.sleep(.5)     
+    return 0
+
+   
+if __name__ == '__main__':
+    """
+    This tests the ouijabot proxy class for high level control
+    this will simply move the robot around to test each directon"""
+    rospy.init_node('ouijabotProxy', anonymous=True)
+    ouijabotTest = OuijabotProxy(rospy.get_param('~poseTopic'), 
+                                 rospy.get_param('~velTopic'))
+    readInput = ""
+    while readInput != "x":
+        readInput = raw_input("enter x to start: ")
+    ouijabotTest.setEnable(True)
+
+    testVelocity(ouijabotTest)
+
+
+    sys.exit()
+
+
