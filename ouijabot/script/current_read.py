317c0e59e58a19740bfaf51919846aff9aefd2a3
Preston Culbertson <pculbertson@stanford.edu>
2019-09-23 14:00:02 -0700

    added current read

---
+++ ouijabot/script/current_read.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import rospy
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+from std_msgs.msg import Float64MultiArray
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+ads = ADS.ADS1015(i2c)
+
+chan0 = AnalogIn(ads,ADS.P0)
+chan1 = AnalogIn(ads,ADS.P1)
+chan2 = AnalogIn(ads,ADS.P2)
+chan3 = AnalogIn(ads,ADS.P3)
+
+if __name__=="__main__":
+	rospy.init_node('adc_test')
+	pub = rospy.Publisher('current',Float64MultiArray,queue_size=10)
+	r = rospy.Rate(100)
+	while not rospy.is_shutdown():
+		msg = Float64MultiArray(data=[chan0.voltage,chan1.voltage,chan2.voltage,chan3.voltage])
+		pub.publish(msg)
+		r.sleep()
